6cfe987d481d7a441d16ad063f95cc9412ac0b3d
Patrockanite <patrickannoot@orange.fr>
2025-10-04 13:09:41 +0200

    Plaque de cuisson

---
+++ SITE/JS/cookies.js
@@ -0,0 +1,10 @@
+window.axeptioSettings = {
+    clientId: "629e2d1f35fa0d5baf75b6f4",
+    cookiesVersion: "grands-pins-fr",
+  };
+   
+  (function(d, s) {
+    var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+    e.async = true; e.src = "//static.axept.io/sdk.js";
+    t.parentNode.insertBefore(e, t);
+  })(document, "script");
